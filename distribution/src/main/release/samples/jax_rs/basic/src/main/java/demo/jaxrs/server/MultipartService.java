81589f825f6943662267f6b52d849c7804073c85
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2023-08-03 11:34:32 +0300

    chore: Improve mock method to save attachments

--- distribution/src/main/release/samples/jax_rs/basic/src/main/java/demo/jaxrs/server/MultipartService.java
+++ distribution/src/main/release/samples/jax_rs/basic/src/main/java/demo/jaxrs/server/MultipartService.java
@@ -1,10 +1,13 @@
 package demo.jaxrs.server;
 
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.StringWriter;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
@@ -24,11 +27,11 @@ public class MultipartService {
         return Response.ok("Abc").build();
     }
 
-    @POST
+    @PUT
     @Path("/attachmentsDemo/")
     public Response addAttachmentsDemo(MultipartBody multipartBody) throws Exception {
         StringBuilder body = new StringBuilder();
-        body.append("Main Body type").append(multipartBody.getType()).append("\r\n");
+        body.append("Main Body type ").append(multipartBody.getType()).append("\r\n");
 
         body.append("<h3>Attachments (").append(multipartBody.getAllAttachments().size()).append(")</h3>\r\n");
 
@@ -37,11 +40,16 @@ public class MultipartService {
             body.append("Content id ").append(at.getContentId()).append("\r\n");
             body.append("Content name ").append(at.getDataHandler().getName()).append("\r\n");
             InputStream in = at.getDataHandler().getInputStream();
+            OutputStream os = new FileOutputStream("C:/dataout/httpWrite/" + at.getDataHandler().getName());
             StringBuilder content = new StringBuilder();
             int i;
             while ((i = in.read()) != -1) {
                 content.append((char) i);
+                os.write(i);
             }
+            in.close();
+            os.flush();
+            os.close();
             body.append("Content ").append(content).append("\r\n");
 
             body.append("\r\n");
